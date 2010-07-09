4c59f8672b2e99133103a7f4d7a22f28bfa75f04
Woody Peterson <woody.peterson@gmail.com>
2010-07-09 13:28:24 -0700

    Code Completion, Errors/Warnings Dialog, Open EShell
    Not my work, just applied the patch from
    http://www.mail-archive.com/textmate-dev@lists.macromates.com/msg06130.html

---
+++ Support/completion/src/tm_menu.erl
@@ -0,0 +1,16 @@
+-module(tm_menu).
+-export([selection/1]).
+-include_lib("xmerl/include/xmerl.hrl").
+
+selection([]) -> "";
+selection(MenuItems) ->
+    %% TODO: need better way to get path for tm_dialog
+    PlistXml = os:cmd(io_lib:format("/Applications/TextMate.app/Contents/PlugIns/Dialog.tmplugin/Contents/Resources/tm_dialog -u -p \"~s\"", [ascii_plist(MenuItems)])),
+    {PlistDoc, _Rest} = xmerl_scan:string(PlistXml),
+    selected( xmerl_xpath:string("/plist/dict/dict[preceding-sibling::key[1]='selectedMenuItem']/string[preceding-sibling::key[1]='title']/text()", PlistDoc)).
+
+selected([#xmlText{value=Selected}]) -> Selected;
+selected([]) ->"".
+
+ascii_plist(MenuItems) ->
+    ["{menuItems = (", [io_lib:format("{title = ~s;},", [Item]) || Item <- MenuItems], ");}"].
